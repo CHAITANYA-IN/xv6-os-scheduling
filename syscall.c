2a1ccbbb8f2b7bb2aad7e1d014bff96a7a5ac376
Amey Makarand <ameymakarand@gmail.com>
2021-04-11 19:47:15 +0530

    Changed arguments

--- syscall.c
+++ syscall.c
@@ -104,7 +104,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_ps(void);
-extern int sys_changepriority(int, int);
+extern int sys_changepriority(void);
 
 static int (*syscalls[])(void) = {
   [SYS_fork]    sys_fork,
