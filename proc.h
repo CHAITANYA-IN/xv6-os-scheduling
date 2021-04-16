018e8477de81d2be36adb81e3d25e032f4857140
Amey Makarand <ameymakarand@gmail.com>
2021-04-17 03:33:10 +0530

    Created Queue Struct

--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int index;                  // Priority of the process (From the Priority Scheduling Algo)
 };
 
 // Process memory is laid out contiguously, low addresses first:
@@ -56,3 +57,11 @@ struct proc {
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
+struct feedback_queue {
+  struct proc *arrq[NPROC];
+  int front;
+  int rear;
+  int max_waiting_time;
+  int max_respone_time;
+
+};
